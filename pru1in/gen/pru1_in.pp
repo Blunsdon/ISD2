0a3ae4814ca149637f7905850411ab49159c9a20
root <root@beaglebone.localdomain>
2020-03-19 23:05:57 +0100

    removed old files

--- pru1in/gen/pru1_in.pp
+++
@@ -1,7 +0,0 @@
-gen/pru1_in.object: pru1_in.c
-gen/pru1_in.object: /usr/share/ti/cgt-pru/include/stdint.h
-gen/pru1_in.object: /usr/lib/ti/pru-software-support-package/include/am335x/pru_cfg.h
-gen/pru1_in.object: resource_table_empty.h
-gen/pru1_in.object: /usr/share/ti/cgt-pru/include/stddef.h
-gen/pru1_in.object: /usr/lib/ti/pru-software-support-package/include/rsc_types.h
-gen/pru1_in.object: /usr/lib/ti/pru-software-support-package/include/pru_types.h
